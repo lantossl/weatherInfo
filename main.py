5a7763da982b111694a14790a717bd31c33a3d4d
LantosSlimbarsz <24784366-LantosSlimbarsz@users.noreply.replit.com>
2023-07-20 11:27:10 +0000

    The time values is in UTC now.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 import os
 import openai
+import datetime
 
 # Set your OpenAI API key here
 api_key = os.environ['OPENWEATHERMAP_API_KEY']
@@ -47,30 +48,37 @@ def generate_weather_description(weather_data):
 
     return response['choices'][0]['text']
 
-# Function to generate weather advice using the gpt-3.5-turbo model
+# Function to generate weather advice for activities and clothing
 def generate_weather_advice(weather_data):
-    # Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
-    openai.api_key = openai_api_key
+    # Convert sunrise and sunset timestamps to human-readable format
+    sunrise_timestamp = weather_data['sys']['sunrise']
+    sunset_timestamp = weather_data['sys']['sunset']
+    sunrise_time = datetime.datetime.fromtimestamp(sunrise_timestamp).strftime('%H:%M')
+    sunset_time = datetime.datetime.fromtimestamp(sunset_timestamp).strftime('%H:%M')
 
-    # Construct the prompt using the weather data
-    prompt = f"The weather in {weather_data['name']} is {weather_data['weather'][0]['description']} with a temperature of {weather_data['main']['temp']} degrees Celsius. It feels like {weather_data['main']['feels_like']} degrees Celsius. The humidity is {weather_data['main']['humidity']}%. Based on the weather conditions, I would suggest the following activities:"
+    # Generate weather advice based on the weather conditions
+    advice = f"For today's weather in {weather_data['name']}, the weather is {weather_data['weather'][0]['description']} with a temperature of {weather_data['main']['temp']} degrees Celsius. The minimum temperature today is {weather_data['main']['temp_min']} degrees Celsius, and the maximum temperature is {weather_data['main']['temp_max']} degrees Celsius. It feels like {weather_data['main']['feels_like']} degrees Celsius. The atmospheric pressure is {weather_data['main']['pressure']} hPa, visibility is {weather_data['visibility']} meters, and wind speed is {weather_data['wind']['speed']} m/s coming from {weather_data['wind']['deg']} degrees. Cloudiness is at {weather_data['clouds']['all']}%."
 
-    # Generate weather advice using gpt-3.5-turbo model
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # You can use gpt-3.5-turbo model
-        prompt=prompt,
-        max_tokens=500,  # Adjust the number of tokens as per your desired length
-        stop=["."]  # Stop generation at the end of a sentence
-    )
+    # Add advice for activities and clothing based on weather conditions
+    if 'rain' in weather_data['weather'][0]['description'].lower():
+        advice += " It may rain today, so don't forget to bring an umbrella or raincoat with you!"
+    else:
+        advice += " It's a good day for outdoor activities. Enjoy the weather!"
 
-    return response['choices'][0]['text']
+    if weather_data['main']['temp'] > 25:
+        advice += " The temperature is quite warm today. Wear light and breathable clothing to stay comfortable."
+    else:
+        advice += " The temperature is moderate today. Dress accordingly and bring a light jacket if needed."
+
+    # Return the weather advice
+    return advice
 
 # Flask app route to show weather information
 @app.route('/')
 def show_weather():
     # Replace 'OPENWEATHERMAP_API_KEY' with the name of your environment variable
     api_key = os.environ.get('OPENWEATHERMAP_API_KEY')
-
+    
     # If USE_CURRENT_COORDINATES is True, use the current coordinates (to be implemented later)
     # Otherwise, use the static coordinates for Budapest
     if USE_CURRENT_COORDINATES:
@@ -102,30 +110,17 @@ def show_weather():
         # Generate dynamic weather description using the OpenAI API
         weather_description_ai = generate_weather_description(weather_data)
 
-        # Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
-        openai.api_key = openai_api_key
-
-        # Construct the prompt using the weather data
-        prompt = f"The weather in {city} is {weather_description_json} with a temperature of {temperature_celsius} degrees Celsius. It feels like {feels_like_celsius} degrees Celsius. The minimum temperature today is {temp_min_celsius} degrees Celsius, and the maximum temperature is {temp_max_celsius} degrees Celsius. The atmospheric pressure is {pressure_hpa} hPa, visibility is {visibility_meters} meters, and wind speed is {wind_speed} m/s coming from {wind_degrees} degrees. Cloudiness is at {cloudiness_percent}%."
-
-        # Generate dynamic weather description using gpt-3.5-turbo model
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # You can use gpt-3.5-turbo model
-            prompt=prompt,
-            max_tokens=500,  # Adjust the number of tokens as per your desired length
-            stop=["."]  # Stop generation at the end of a sentence
-        )
-
-        # Get the generated weather description from the API response
-        weather_description_ai = response['choices'][0]['text']
-        # Generate weather advice using the OpenAI API
+        # Generate weather advice for activities and clothing
         weather_advice = generate_weather_advice(weather_data)
+
+        # Convert sunrise and sunset timestamps to human-readable format
+        sunrise_time = datetime.datetime.fromtimestamp(sunrise_timestamp).strftime('%H:%M')
+        sunset_time = datetime.datetime.fromtimestamp(sunset_timestamp).strftime('%H:%M')
 
         # Render the template with weather data and separate descriptions
         return render_template('weather.html', city=city, temperature=temperature_celsius,
                                weather_description_json=weather_description_json,
                                weather_description_ai=weather_description_ai,
-                               humidity=humidity, wind_speed=wind_speed,
                                feels_like_celsius=feels_like_celsius,
                                temp_min_celsius=temp_min_celsius,
                                temp_max_celsius=temp_max_celsius,
@@ -133,9 +128,10 @@ def show_weather():
                                visibility_meters=visibility_meters,
                                wind_degrees=wind_degrees,
                                cloudiness_percent=cloudiness_percent,
-                               sunrise_timestamp=sunrise_timestamp,
-                               sunset_timestamp=sunset_timestamp,
-                               weather_advice=weather_advice)
+                               sunrise_time=sunrise_time,
+                               sunset_time=sunset_time,
+                               weather_advice=weather_advice,
+                               humidity=humidity, wind_speed=wind_speed)
     else:
         return "Weather data not available."
 
